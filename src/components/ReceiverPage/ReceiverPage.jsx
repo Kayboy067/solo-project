b4d2ba9b1adda5f273a83e494516602345c4c22b
Kayode Adebanjo <kay.adebanjo@gmail.com>
2022-02-18 00:06:45 -0600

    fixed preview page and estimate page button

--- src/components/ReceiverPage/ReceiverPage.jsx
+++ src/components/ReceiverPage/ReceiverPage.jsx
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
 import ReceiverForm from '../ReceiverForm/ReceiverForm';
 import ReceiverDetail from './ReceiverDetail'
-import { useSelector, useDispatch } from 'react-redux';
 
 
 function Receiver() {
-    const dispatch = useDispatch();
    
 
  
@@ -14,9 +11,6 @@ function Receiver() {
         <h1>Enter Receiver Information</h1>
         <ReceiverForm />
        <ReceiverDetail />
-       <center>
-        <Link to="/disclaimer"><button className='btn' >continue</button></Link>
-        </center>
     </>
     )
 }
